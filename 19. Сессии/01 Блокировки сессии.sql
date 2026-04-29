984d882e6bcf7c9d57f6b7643efa5362b400c50e
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2026-04-29 19:16:19 +0300

    Update 01 Блокировки сессии.sql

--- 19. Сессии/01 Блокировки сессии.sql
+++ 19. Сессии/01 Блокировки сессии.sql
@@ -1,16 +1,11 @@
--- Порядок действий
--- 1. Найти блокирующую сессию
--- 2. Посмотреть что за PLSQL_ENTRY_OBJECT_ID который вызвал блокировку строк. 
--- 3. Обычный запрос не может блокировать строки. Возможно до этого пользователь внес изменения в таблицах и заблокировал строки. 
--- 4. После чего пошел выполнять какой нибудь тяжелый запрос. Оставив строки заблокированными. 
--- 5. Нужно позвонить пользователю и спросить что он сейчас делает.
--- 6. Убить сессию.
 
+-- 01. Сколько всего блокировок
 select count(*)
   from v$session v
 where v.username is not null
   and v.FINAL_BLOCKING_SESSION is not null;
 
+-- 02. Кто сколько блокирует
 select count(*),
        v.FINAL_BLOCKING_SESSION
   from v$session v
@@ -19,7 +14,7 @@ where v.username is not null
 group by v.FINAL_BLOCKING_SESSION;
 
 
-
+-- 03. Все о блокирующей сессии
 select v.sid,
        v.CLIENT_INFO,
        v.STATUS,
@@ -27,23 +22,26 @@ select v.sid,
        l.SQL_TEXT,
        (select o.OBJECT_NAME
           from all_objects o
-         where o.OBJECT_ID = v.PLSQL_ENTRY_OBJECT_ID) OBJECT_NAME,
-       v.PLSQL_ENTRY_OBJECT_ID
+         where o.OBJECT_ID = v.PLSQL_ENTRY_OBJECT_ID) ENTRY_OBJECT_NAME,
+       (select p.PROCEDURE_NAME
+          from all_procedures p
+         where p.SUBPROGRAM_ID = v.PLSQL_ENTRY_SUBPROGRAM_ID
+           and p.object_id = v.PLSQL_ENTRY_OBJECT_ID) ENTRY_PROCEDURE,
+       v.PLSQL_ENTRY_OBJECT_ID,
+       v.PLSQL_ENTRY_SUBPROGRAM_ID,
+       (select o.OBJECT_NAME
+          from all_objects o
+         where o.OBJECT_ID = v.PLSQL_OBJECT_ID) CURRENT_OBJECT_NAME,
+         v.PLSQL_OBJECT_ID
   from v$session v,
        v$sql l
 where v.username is not null
-  and v.SID = 8734
+  and v.SID = 5873
   --
   and l.SQL_ID(+) = v.SQL_ID
 order by v.FINAL_BLOCKING_SESSION nulls last
 
 
+-- 03. Информацию собрали, теперь убиваем. Ждем когда откатится транзакция и только после этого начнут сниматься блокировки.
 ALTER SYSTEM KILL SESSION '1528,55325' IMMEDIATE;
 
-
-SELECT v.TYPE, 
-       v.STATE, 
-       v.NUM_BLOCKED, 
-       v.CLEANUP_ATTEMPTS
-  FROM V$DEAD_CLEANUP v
- WHERE PADDR IN (SELECT PADDR FROM V$SESSION WHERE STATUS = 'KILLED');
